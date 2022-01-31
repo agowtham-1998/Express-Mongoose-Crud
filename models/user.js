153bbeac7dca4c8327f5315f90c2274ef51f6588
gowtham <agowtham8991@gmail.com>
2022-02-01 04:01:36 +0530

    crud

---
+++ models/user.js
@@ -0,0 +1,78 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * User Schema
+ */
+var userSchema = new Schema({
+  fullName: {
+    type: String,
+    required: [true, "fullname not provided "],
+  },
+  email: {
+    type: String,
+    unique: [true, "email already exists in database!"],
+    lowercase: true,
+    trim: true,
+    required: [true, "email not provided"],
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: '{VALUE} is not a valid email!'
+    }
+
+  },
+  role: {
+    type: String,
+    enum: ["normal", "admin"],
+    required: [true, "Please specify user role"]
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+var express = require("express"),
+  router = express.Router(),
+  verifyToken = require('../middlewares/authJWT'),
+  {
+    signup,
+    signin
+  } = require("../middlewares/auth");
+
+router.post("/register", signup, function (req, res) {
+
+});
+
+router.post("/login", signin, function (req, res) {
+
+});
+
+router.get("/hiddencontent", verifyToken, function (req, res) {
+  if (!user) {
+    res.status(403)
+      .send({
+        message: "Invalid JWT token"
+      });
+  }
+  if (req.user == "admin") {
+    res.status(200)
+      .send({
+        message: "Congratulations! but there is no hidden content"
+      });
+  } else {
+    res.status(403)
+      .send({
+        message: "Unauthorised access"
+      });
+  }
+});
+
+module.exports = router;
+
+module.exports = mongoose.model('User', userSchema);
